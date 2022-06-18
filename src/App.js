615e3432a03935f0f0c8bf04be6330b6f58f3e21
Smiley <jcsmileyjr@gmail.com>
2022-06-17 19:26:47 -0500

    Initial weigthedOptions button group

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import WeightedOption from './components/weightedOption/WeightedOption';
 
 function App() {
   return (
     <main>
-
+      <h1>How was your day?</h1>
+      <p>A day can be like a story; with a begining, a journey in the middle, and an end. I want to know what happen to you today. The good, bad, funny, and the ugly.</p>
+      <WeightedOption />
+      <button type="button">Send</button>
     </main>
   );
 }
---
+++ src/components/weightedOption/WeightedOption.js
@@ -0,0 +1,20 @@
+import './weightedOption.css';
+
+const weightedOption = ({}) => {
+    let radioOptions = [];
+
+    for(let i=0;i<=4;i++){
+        let newID = `weightedOption${i}`
+        radioOptions.push(
+          <button id={newID} type="button" className='weightedOption__button--style'>{i}</button>
+        )
+    }
+
+    return (
+        <section className='weightedOption__buttonPanel--container'>
+            {radioOptions}
+        </section>
+    )
+}
+
+export default weightedOption;
