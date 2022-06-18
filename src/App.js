6d7e3cc0af97305bf0e5c5055dc7f7aa1d7c045b
Smiley <jcsmileyjr@gmail.com>
2022-06-17 20:19:47 -0500

    Style change to the button and text changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import WeightedOption from './components/weightedOption/WeightedOption';
 function App() {
   return (
     <main>
-      <h1>How was your day?</h1>
-      <p>Every day is a different. Some days are a blessing and others feel like a curse. I want to know how was your day today?</p>
-      <p>Good, bad, funny, or ugly; I care!</p>
+      <h1>How was your day 🧐</h1>
+      <p>Every day is different. Some days are a blessing and others feel like a curse. I want to know how was your day today?</p>
       <WeightedOption />
       <button type="button" className='main__button--style'>Send</button>
     </main>
