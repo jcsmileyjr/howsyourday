9ca68325f0a00175e2c005b258dcac4d69d74d83
Smiley <jcsmileyjr@gmail.com>
2022-06-27 06:28:59 -0500

    Small style changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import React, {useState} from 'react';
 function App() {
 
   return (
-    <main>
-      <h1>How was your day</h1>
-      <p>Every day is different. Some days are a blessing and others feel like a curse. I want to know how was your day today?</p>
-      <WeightedOption  />
-      <button type="button" className='main__button--style'>SEND</button>
-    </main>
+    <div className='UI-background'>
+      <main>
+        <h1>How was your day</h1>
+        <p>Every day is different. Some days are a blessing and others feel like a curse. I want to know how was your day today?</p>
+        <WeightedOption  />
+        <button type="button" className='main__button--style'>SEND</button>
+      </main>
+    </div>
   );
 }
 
