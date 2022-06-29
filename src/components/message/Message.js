f3f4c61ada7cc89840c2c85e6e8ab8e5a9c960d6
Smiley <jcsmileyjr@gmail.com>
2022-06-29 16:59:37 -0500

    Added Go Back button functionality and UI

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,11 +1,16 @@
 import "./message.css";
 import "../../App.css";
+import GoBack from "../../images/undo-circle-outline.png";
 
 const Message = ({comment1,comment2, getChoice}) => {
     return(
         <section className="message--container">
+            <div className="message__img--container">
+                <button type="button" className="message__imgButton--style" onClick={() => getChoice("")}>
+                    <img className="message__img--style" src={GoBack} alt="" />
+                </button>
+            </div>
             <h1 className="message__header--container">{comment1} <span className="messsage__secondLine--style">{comment2}</span></h1>
-            
             <textarea rows="10" />
             <button className="main__button--style" onClick={() => getChoice(6)} type="button" >Send message to Daddy</button>
             <button className="noComment__button--style" onClick={() => getChoice(7)} type="button">No Comment</button>
