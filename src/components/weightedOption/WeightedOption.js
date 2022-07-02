c27978783009f367bedfcc700f75c39362228e71
Smiley <jcsmileyjr@gmail.com>
2022-07-02 11:30:14 -0500

    adding expressions

--- src/components/weightedOption/WeightedOption.js
+++ src/components/weightedOption/WeightedOption.js
@@ -2,6 +2,10 @@ import './weightedOption.css';
 import React, { useState, useEffect } from "react";
 import ButtonExpressions from '../buttonExpressions/ButttonExpressions';
 import AngelEmojii from '../../images/female-angel-emojii.png';
+import PartyEmojii from '../../images/party-emojii.png';
+import AngryEmojii from '../../images/angry-emojii.png';
+import CryingEmojii from '../../images/crying-alien.png';
+import PanicEmojii from '../../images/panic-emojii.png';
 
 const WeightedOption = ({getChoice}) => {
     const [highlightButton, setHighlightButton] = useState("");
@@ -16,16 +20,16 @@ const WeightedOption = ({getChoice}) => {
         radioOptions.push(
           <button key={i} aria-label={`Rate option ${i}`} onClick={() => {setHighlightButton(newID); getChoice(i)}} id={newID} type="button" className={`weightedOption__button--style ${highlightButton === newID?'highlightButton':''}`}>
             {i === 1 &&
-                <ButtonExpressions optionNumber={i} expressionImage={AngelEmojii} />
+                <ButtonExpressions optionNumber={i} expressionImage={CryingEmojii} />
             }
             {i === 2 &&
-                <ButtonExpressions optionNumber={i} expressionImage={AngelEmojii} />
+                <ButtonExpressions optionNumber={i} expressionImage={AngryEmojii} />
             }
             {i === 3 &&
-                <ButtonExpressions optionNumber={i} expressionImage={AngelEmojii} />
+                <ButtonExpressions optionNumber={i} expressionImage={PanicEmojii} />
             }
             {i === 4 &&
-                <ButtonExpressions optionNumber={i} expressionImage={AngelEmojii} />
+                <ButtonExpressions optionNumber={i} expressionImage={PartyEmojii} />
             }
             {i === 5 &&
                 <ButtonExpressions optionNumber={i} expressionImage={AngelEmojii} />
